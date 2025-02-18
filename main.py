4382e44cf9f831b28a43667375fbda4f8e6e1980
GritandGo <codemain0123@gmail.com>
2025-02-17 22:10:30 -0600

    "Add player movement and controls
    - Add PLAYER_SPEED constant
    - Implement player movement with vector math
    - Add W/S controls for forward/backward movement
    - Fix rotation and position handling"

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import pygame
 from constants import *
+import player as player_module
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    my_ship = player_module.Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        my_ship.update(dt)
+        my_ship.draw(screen)
         pygame.display.flip()
-
+        
     
-
 if __name__ == "__main__":
     main()
